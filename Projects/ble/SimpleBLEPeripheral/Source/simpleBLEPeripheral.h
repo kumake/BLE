49766150ebaa81c6cda69ec8a45c57e835284a2c
kumake <kumake@qq.com>
2015-11-26 23:26:38 +0800

    test

--- Projects/ble/SimpleBLEPeripheral/Source/simpleBLEPeripheral.h
+++ Projects/ble/SimpleBLEPeripheral/Source/simpleBLEPeripheral.h
@@ -34,7 +34,7 @@
   (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
 
   Should you have any questions regarding your right to use this Software,
-  contact Texas Instruments Incorporated at www.TI.com.
+  contact Texas Instruments Incorporated at www.TI.com..
 **************************************************************************************************/
 
 #ifndef SIMPLEBLEPERIPHERAL_H
